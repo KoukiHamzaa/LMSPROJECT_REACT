b7eef13c1d89fcc619ba2e9d9d42dfac77250ba3
khalfikhawla <khawla.khalfi@etudiant-fsegt.utm.tn>
2020-04-04 15:40:45 +0100

    newCommit

--- Test Project/React_Phone_E-Commerce/Store/src/components/Navbar.js
+++ Test Project/React_Phone_E-Commerce/Store/src/components/Navbar.js
@@ -1,13 +1,34 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
+import { Link } from "react-router-dom";
+import logo from "../logo.svg";
+
 export default class Navbar extends Component {
-    state = {  }
-    render() { 
-        return ( 
-            <div>
-                <h3>
-                    Hello from Navbar
-                </h3>
-            </div>
-         );
-    }
+  state = {};
+  render() {
+    return (
+      <nav className="navbar navbar-expand-sm bg-primary navbar-dark px-sm-5">
+        {/* https://www.iconfinder.com/icons/1243689/call_phone_icon Creative
+        Commons (Attribution 3.0 Unported);
+        https://www.iconfinder.com/Makoto_msk */}
+        <Link to="/">
+          <img src={logo} alt="store" className="navbar-brand" />
+        </Link>
+        <ul className="navbar-nav align-items-center">
+          <li className="nav-item ml-5">
+            <Link to="/" className="nav-link">
+              products
+            </Link>
+          </li>
+        </ul>
+        <Link to="/cart" className="ml-auto">
+            <button>
+                <span>
+                    <i className="fas fa-cart-plus"/>
+                    my cart
+                </span>
+            </button>
+        </Link>
+      </nav>
+    );
+  }
 }
