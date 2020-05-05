be4b3a8fedda63194248005a9e3acf852fa5428b
Hamza Kouki <hamzakouki33@gmail.com>
2020-05-05 15:29:21 +0100

    freeCodeGramAddUserNameAndSomeFonts

--- server/freeCodeGram/resources/views/layouts/app.blade.php
+++ server/freeCodeGram/resources/views/layouts/app.blade.php
@@ -8,7 +8,7 @@
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
     <title>{{ config('app.name', 'Laravel') }}</title>
-
+    <!-- <title>freeCodeCamp</title> -->
     <!-- Scripts -->
     <script src="{{ asset('js/app.js') }}" defer></script>
 
@@ -23,8 +23,10 @@
     <div id="app">
         <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
             <div class="container">
-                <a class="navbar-brand" href="{{ url('/') }}">
-                    {{ config('app.name', 'Laravel') }}
+                <a class="navbar-brand d-flex" href="{{ url('/') }}">
+                    <!-- {{ config('app.name', 'Laravel') }} -->
+                    <div><img src="/svg/freeCodeCampLogo.svg" style="height:20px; border-right: 1px solid #333333" class="pr-3"></div>
+                    <div class="pl-3">freeCodeGram</div>
                 </a>
                 <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
                     <span class="navbar-toggler-icon"></span>
