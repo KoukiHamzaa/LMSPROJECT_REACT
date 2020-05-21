4a9b90bae9da5146a03669a870654b773c56a920
Hamza Kouki <hamzakouki33@gmail.com>
2020-05-21 01:16:03 +0100

    newCommit

--- server/lara-course/resources/views/layouts/app.blade.php
+++ server/lara-course/resources/views/layouts/app.blade.php
@@ -2,7 +2,7 @@
 <html>
 <head>
     <meta charset="UTF-8">
-    <title>InfyOm Generator</title>
+    <title>Ulearn</title>
     <meta content='width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no' name='viewport'>
 
     <!-- Bootstrap 3.3.7 -->
