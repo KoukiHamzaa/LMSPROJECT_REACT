382d2425ee6e390d43f7e14794159d003cd37905
Hamza Kouki <hamzakouki33@gmail.com>
2020-06-21 17:21:18 +0100

    creating course menu

---
+++ server/lara-course/resources/views/courses/menu.blade.php
@@ -0,0 +1,5 @@
+<ul class="list-group list-group-horizontal-sm">
+  <li class="list-group-item">Cras justo odio</li>
+  <li class="list-group-item">Dapibus ac facilisis in</li>
+  <li class="list-group-item">Morbi leo risus</li>
+</ul>
