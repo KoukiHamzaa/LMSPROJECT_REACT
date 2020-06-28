d7c13da9ddb56988a8a4bc18210e4c6ffd84796e
Hamza Kouki <hamzakouki33@gmail.com>
2020-06-28 23:20:02 +0100

    Regular auto-commit lara-courseat 23:20:01 on 28/06/2020

--- server/lara-course/resources/views/courses/show_fields.blade.php
+++ server/lara-course/resources/views/courses/show_fields.blade.php
@@ -7,16 +7,13 @@
     <p>{{ $course->description }}</p>
 </div>
 
-<!-- Category Id Field -->
-<div class="form-group col-md-3">
-<div class="mt-5">
-    {!! Form::label('category_id', 'Category name:') !!}
-    <a href="/categories/{!! $course->category['id'] !!}" target="_blank"><p class="h4 text-capitalize text-success"><i class="fa fa-link"  font-weight-bold" aria-hidden="true">{{ $course->category['name'] }}</i></p></a>
-</div>
+<div class="form-group  col-md-3">
+  <img src="{{ $course->photo }}" class="img-fluid" alt="Responsive image">
+    <!-- <img src="" class="card-img-top" alt="Card image cap" width="400" height="200"> -->
 </div>
 
 <!-- About Instructor Field -->
-<div class="form-group col-md-6 list-group-item list-group-item-info m-0">
+<div class="form-group col-md-9">
     {!! Form::label('about_instructor', 'About Instructor:') !!}
     <p>{{ $course->about_instructor }}</p>
 </div>
@@ -28,48 +25,50 @@
     allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen width="400" height="200"></iframe>
 </div>--}}
 
-<!-- Photo Field -->
-<div class="form-group  list-group-item-warning">
-  <img src="{{ $course->photo }}" class="img-fluid" alt="Responsive image">
-    <!-- <img src="" class="card-img-top" alt="Card image cap" width="400" height="200"> -->
+<!-- Category Id Field -->
+<div class="form-group col-md-3">
+    <div class="mt-5">
+        {!! Form::label('category_id', 'Category name:') !!}
+        <a href="/categories/{!! $course->category['id'] !!}" target="_blank"><p class="h4 text-capitalize text-success"><i class="fa fa-link"  font-weight-bold" aria-hidden="true">{{ $course->category['name'] }}</i></p></a>
+    </div>
 </div>
 
 <!-- Tags Field -->
-<div class="form-group col-md-12 list-group-item list-group-item-info">
+<div class="form-group col-md-6">
     {!! Form::label('tags', 'Tags:') !!}
     <p>{{ $course->tags }}</p>
 </div>
 
 <!-- created time -->
-<div class="form-group col-md-4 list-group-item disabled">
+<div class="form-group col-md-3">
 <div class="mt-5">
     {!! Form::label('created_at', 'Date de création du cours :') !!}
     <p>{{ $course->created_at->format('h:i a - D d M Y') }}</p>
 </div>
 </div>
 
-<!-- What Will Student Learn Field -->
-<div class="form-group col-md-8 list-group-item list-group-item-info">
-    {!! Form::label('what_will_student_learn', 'What Will Student Learn:') !!}
-    <p>{{ $course->what_will_student_learn }}</p>
-</div>
-
 <!-- created time -->
-<div class="form-group col-md-4 list-group-item disabled">
+<div class="form-group col-md-3">
     <div class="mt-5">
         {!! Form::label('updated_at', 'dernière mise à jour du cours :') !!}
         <p>{{ $course->updated_at->format('h:i a - D d M Y') }}</p>
     </div>
 </div>
 
+<!-- What Will Student Learn Field -->
+<div class="form-group col-md-8">
+    {!! Form::label('what_will_student_learn', 'What Will Student Learn:') !!}
+    <p>{{ $course->what_will_student_learn }}</p>
+</div>
+
 <!-- Target Student Field -->
-<div class="form-group col-md-12 list-group-item list-group-item-info">
+<div class="form-group col-md-12">
     {!! Form::label('target_student', 'Target Student:') !!}
     <p>{{ $course->target_student }}</p>
 </div>
 
 <!-- Requirements Field -->
-<div class="form-group col-md-12 list-group-item list-group-item-info">
+<div class="form-group col-md-12 ">
     {!! Form::label('requirements', 'Requirements:') !!}
     <p>{{ $course->requirements }}</p>
 </div>
