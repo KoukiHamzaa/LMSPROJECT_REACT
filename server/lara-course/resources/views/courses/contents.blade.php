f9d2d11996cc36b55a71195216b341aeb5daf984
Hamza Kouki <hamzakouki33@gmail.com>
2020-06-29 01:00:02 +0100

    Regular auto-commit lara-courseat 01:00:01 on 29/06/2020

--- server/lara-course/resources/views/courses/contents.blade.php
+++ server/lara-course/resources/views/courses/contents.blade.php
@@ -1,2 +1,7 @@
-
+<!-- Playlist Url Field -->
+{{--<div class="form-group col-md-6 list-group-item-warning m-0">
+    {!! Form::label('playlist_url', 'Playlist Url:') !!}<br>
+    <iframe  src="{{ $course->playlist_url }}" frameborder="0" 
+    allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen width="400" height="200"></iframe>
+</div>--}}
 
