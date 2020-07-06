6420ab62d14662f88804a330a699c0128ba1e4d9
Hamza Kouki <hamzakouki33@gmail.com>
2020-07-06 21:39:37 +0100

    update

--- ulearn/resources/views/emails/payments/payment_submitted_admin.blade.php
+++
@@ -1,3 +0,0 @@
-Someone just made payment on  <a href="https://braintem.org/courses/{{ $payment->course_id }}">
-   {{ $payment->course['title'] }}</a> <br/>
-Go and approve it.
--- ulearn/resources/views/emails/users/signup.blade.php
+++
@@ -1,3 +0,0 @@
-Thanks for signing up on <a href="https://braintem.org">Braintem.org</a>
-
-Do well to always check back for new courses. 
